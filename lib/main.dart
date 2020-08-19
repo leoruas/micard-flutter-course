30ff47032d61816e7010da56b6320776c6c3f565
leoruas <leo.coelho.ruas@gmail.com>
2020-08-19 18:13:35 -0300

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey[400],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage("images/mePic.png"),
+                backgroundColor: Colors.blueGrey.shade200
+              ),
+              Text(
+                '~ Lheu Ruahs ~',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER STUDENT',
+                style: TextStyle(
+                    fontFamily: 'SourceSans',
+                    fontSize: 16,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 120.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.blueGrey.shade400),
+                  title: Text(
+                    '+55 11 1234-1234',
+                    style: TextStyle(fontFamily: 'SourceSans', fontSize: 18.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.blueGrey.shade400),
+                  title: Text(
+                    'email@email.com',
+                    style: TextStyle(fontFamily: 'SourceSans', fontSize: 18.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
